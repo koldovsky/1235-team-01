e606bcc417c9dd92372c87de9c7c174a4d7cabc8
Dmitry-Cherkasov <105354454+Dmitry-Cherkasov@users.noreply.github.com>
2024-07-20 11:56:50 +0300

    Create tasks-02.js

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+//Write a function to split a string and convert it into an array of words.
+function stringToArray(string) {
+  return string.split(" ");
+}
