993768c7098b5bb472a5dcbd51a87781cf5ac197
Dmitry-Cherkasov <105354454+Dmitry-Cherkasov@users.noreply.github.com>
2024-07-20 12:03:47 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,6 +1,11 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-
 //Write a function to split a string and convert it into an array of words.
 function stringToArray(string) {
   return string.split(" ");
+}
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+// create a function which returns an RNA sequence from the given DNA sequence
+function DNAtoRNA(dna) {
+  return dna.replaceAll("T", "U");
 }
